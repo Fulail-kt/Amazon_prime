02723fb19042e8abaf80411d4e8ec32c307eb142
Fulal-kt <muhamedfulail77@gmail.com>
2024-05-17 01:30:50 +0530

    navbar aligned

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -2,43 +2,45 @@ import React from 'react';
 import Image from 'next/image';
 import search from '../../../public/images/search.png'
 import profile from '../../../public/images/profile.svg'
+import arrow from '../../../public/images/arrow.svg'
+import down from '../../../public/images/down.svg'
 
 
 const Navbar = () => {
   return (
-    <nav className="bg-gray-900 text-white w-full">
+    <nav className="bg-black text-white w-full">
       <div className=" px-4  flex items-center w-full justify-center">
         {/* Logo */}
         <div className="flex items-center">
-          <span className="text-xl font-bold px-6">prime video</span>
+          <span className="text-lg font-semibold px-6">prime video</span>
         </div>
         
         {/* Navigation */}
         <div className="hidden md:flex ">
           <div className="relative group">
-            <button className="hover:text-gray-300 py-1 px-3 hover:bg-black">Home</button>
-            <ul className="absolute left-0 mt-2 hidden group-hover:block bg-gray-800 text-sm rounded-md shadow-lg">
+            <button className="hover:text-white text-sm py-1 px-3 hover:bg-gray-800 flex items-center">Home <span className='group-hover:rotate-180'> <Image src={down} width={15} height={15} alt='arrow'/></span></button>
+            <ul className="absolute left-0  hidden group-hover:block bg-gray-800 text-sm  shadow-lg">
               <li className="block px-4 py-2 hover:bg-gray-700">Option 1</li>
               <li className="block px-4 py-2 hover:bg-gray-700">Option 2</li>
             </ul>
           </div>
           <div className="relative group">
-            <button className="hover:text-gray-300 py-1 px-3 hover:bg-black">Store</button>
-            <ul className="absolute left-0 mt-2 hidden group-hover:block bg-gray-800 text-sm rounded-md shadow-lg">
+            <button className="hover:text-white py-1 px-3 hover:bg-gray-800 flex items-center">Store <span className='group-hover:rotate-180'> <Image src={down} width={15} height={15} alt='arrow'/></span></button>
+            <ul className="absolute left-0 hidden group-hover:block bg-gray-800 text-sm  shadow-lg">
               <li  className="block px-4 py-2 hover:bg-gray-700">Option 1</li>
               <li  className="block px-4 py-2 hover:bg-gray-700">Option 2</li>
             </ul>
           </div>
           <div className="relative group">
-            <button className="hover:text-gray-300 py-1 px-3 hover:bg-black">Live TV</button>
-            <ul className="absolute left-0 mt-2 hidden group-hover:block bg-gray-800 text-sm rounded-md shadow-lg">
+            <button className="hover:text-white py-1 px-3 hover:bg-gray-800 ">Live TV</button>
+            <ul className="absolute left-0  hidden group-hover:block bg-gray-800 text-sm  shadow-lg">
               <li  className="block px-4 py-2 hover:bg-gray-700">Option 1</li>
               <li  className="block px-4 py-2 hover:bg-gray-700">Option 2</li>
             </ul>
           </div>
           <div className="relative group">
-            <button className="hover:text-gray-300 py-1 px-3 hover:bg-black">Categories</button>
-            <ul className="absolute left-0 mt-2 hidden group-hover:block bg-gray-800 text-sm rounded-md shadow-lg">
+            <button className="hover:text-white py-1 px-3 hover:bg-gray-800 flex items-center">Categories <span className='group-hover:rotate-180 '> <Image src={down} width={15} height={15} alt='arrow'/></span></button>
+            <ul className="absolute left-0  hidden group-hover:block bg-gray-800 text-sm  shadow-lg">
               <li  className="block px-4 py-2 hover:bg-gray-700">Option 1</li>
               <li  className="block px-4 py-2 hover:bg-gray-700">Option 2</li>
             </ul>
@@ -46,27 +48,27 @@ const Navbar = () => {
         </div>
 
         {/* Right side */}
-        <div className="flex items-center space-x-6">
+        <div className="flex items-center pl-5 ">
+          
+            <div className="flex items-center  hover:bg-gray-800 h-full">
+              <Image src={search} width={15} height={15} alt='search'/>
+            </div>
+          
           <div>
-            <button className="focus:outline-none">
-              <Image src={search} width={10} height={10} alt='search'/>
-            </button>
-          </div>
-          <div>
-            <span  className="hover:text-gray-300">Try for free</span>
+            <span  className="hover:text-white px-3 hover:bg-gray-800">Try for free</span>
           </div>
           <div className="relative group">
-            <button className="hover:text-gray-300">EN</button>
-            <ul className="absolute right-0 mt-2 hidden group-hover:block bg-gray-800 text-sm rounded-md shadow-lg">
+            <button className="hover:text-white px-3 hover:bg-gray-800 flex items-center">EN <span className='group-hover:rotate-180'> <Image src={down} width={15} height={15} alt='arrow'/></span></button>
+            <ul className="absolute right-0  hidden group-hover:block bg-gray-800 text-sm rounded-md shadow-lg">
               <li className="block px-4 py-2 hover:bg-gray-700">English</li>
               <li className="block px-4 py-2 hover:bg-gray-700">Spanish</li>
             </ul>
           </div>
           <div>
             {/* user profile */}
-            <button>
+            <div className='flex items-center px-3  hover:bg-gray-800 h-full'>
                 <Image src={profile} alt='profile'/>
-            </button>
+            </div>
           </div>
         </div>
       </div>
