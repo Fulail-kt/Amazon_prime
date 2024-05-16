fb820b9363aa4f0fe4c386376e88277550a47020
Fulal-kt <muhamedfulail77@gmail.com>
2024-05-16 23:53:16 +0530

    3rd section added

---
+++ src/app/constants/constant.jsx
@@ -0,0 +1,28 @@
+import lionsgate from '../../../public/images/lionsgate.jpeg'
+import discovery from '../../../public/images/discovery.jpg'
+import bbc from '../../../public/images/bbc.jpeg'
+import manorama from '../../../public/images/manorama.jpg'
+import hoichoi from '../../../public/images/hoichoi.jpg'
+import chaupal from '../../../public/images/chaupal.jpg'
+import anime from '../../../public/images/anime.jpg'
+import sony from '../../../public/images/sony.jpg'
+import mgm from '../../../public/images/mgm.jpg'
+import vrott from '../../../public/images/vrott.jpg'
+import erosnow from '../../../public/images/erosnow.jpg'
+import stingray from '../../../public/images/stingray.jpg'
+
+
+export const images={
+    img:lionsgate,
+    img:discovery,
+    img:bbc,
+    img:manorama,
+    img:hoichoi,
+    img:chaupal,
+    img:anime,
+    img:sony,
+    img:mgm,
+    img:vrott,
+    img:erosnow,
+    img:stingray,
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,34 @@
 import Image from "next/image";
 import Navbar from "./components/navbar";
-import img from '../../public/images/herobg.jpg'
+// import { images } from "./constants/constant";
+
+import lionsgate from '../../public/images/lionsgate.jpg'
+import discovery from '../../public/images/discovery.jpg'
+import bbc from '../../public/images/bbc.png'
+import manorama from '../../public/images/manorama.jpg'
+import hoichoi from '../../public/images/hoichoi.jpg'
+import chaupal from '../../public/images/chaupal.png'
+import anime from '../../public/images/anime.jpg'
+import sony from '../../public/images/sony.png'
+import mgm from '../../public/images/mgm.png'
+import vrott from '../../public/images/vrott.png'
+import erosnow from '../../public/images/erosnow.jpg'
+import stingray from '../../public/images/stingray.png'
+
+const images=[
+  {img:lionsgate},
+  {img:discovery},
+  {img:bbc},
+  {img:manorama},
+  {img:hoichoi},
+  {img:chaupal},
+  {img:anime},
+  {img:sony},
+  {img:mgm},
+  {img:vrott},
+  {img:erosnow},
+  {img:stingray},
+]
 export default function Home() {
   return (
     <main className=" min-h-screen ">
@@ -17,14 +45,27 @@ export default function Home() {
     </div>
 
 
-    <div className="h-screen justify-start bg-hero-bg2 bg-no-repeat flex-1 bg-right w-full">
+    <div className="h-screen overflow-y-hidden justify-start bg-hero-bg2 bg-no-repeat flex-1 bg-right w-full">
       <div className="bg-black-gradient h-full flex tracking-wide flex-col gap-y-8 p-10 w-full ">
          <h1 className="text-4xl ">Movie rentals on Prime Video</h1>
          <p className="text-xl">Join Prime to watch the latest movies, TV shows and award- <br/> winning Amazon Originals</p>
          <div className="py-4"><button className="bg-blue-500 px-14 py-3 rounded-sm">sign in to join Prime</button></div>
       </div>
     </div>
-
+    <div className="bg-white flex px-16 text-black h-100  w-full">
+       <div className="w-1/2 py-10  flex flex-col gap-6">
+        <h1 className="text-[38px] font-extrabold" >Your favorite channels all <br/> in one place</h1>
+        <p className="text-xl font-medium tracking-wider">With Prime Video Channels, find shows and <br/> movies from your favorite channels all in one<br/> place. Enjoy with an add-on subscription to<br/> Channels of your choice</p>
+       </div>
+       <div className="w-1/2">
+       <div className="grid grid-cols-3 p-2 gap-1">
+        {images.map((item,index)=>(
+          <Image key={index} className="hover:shadow-lg shadow hover:shadow-black " src={item?.img}/>
+       ))}
+       </div>
+       </div>
+       
+    </div>
    
 
     </main>
